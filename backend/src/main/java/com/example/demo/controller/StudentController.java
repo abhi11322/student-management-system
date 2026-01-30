c15f74e7b1b0616167d112f2a72273e8e3e5f0c0
Abhishek Poovaiah M <abhishekpoovaiah113@gmail.com>
2026-01-30 20:50:23 +0530

    Add crud operations

---
+++ backend/src/main/java/com/example/demo/controller/StudentController.java
@@ -0,0 +1,39 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.Student;
+import com.example.demo.service.StudentService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/students")
+@CrossOrigin(origins = "http://localhost:5173")
+public class StudentController {
+
+    private final StudentService service;
+
+    public StudentController(StudentService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public Student createStudent(@RequestBody Student student) {
+        return service.createStudent(student);
+    }
+
+    @GetMapping
+    public List<Student> getAllStudents() {
+        return service.getAllStudents();
+    }
+
+    @GetMapping("/{id}")
+    public Student getStudentById(@PathVariable Long id) {
+        return service.getStudentById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteStudent(@PathVariable Long id) {
+        service.deleteStudent(id);
+    }
+}
